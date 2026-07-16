d2b3fc631786a67504df37869893ab4002571258
hamody1818 <hamodycatli18@gmail.com>
2026-07-16 16:11:44 +0800

    Add files via upload

---
+++ hamody2d/age.php
@@ -0,0 +1,18 @@
+<?php
+$age = 70;
+
+if ($age >= 12 ) {
+    echo "child";
+}
+elseif($age >= 17){
+    echo "teenager";
+}
+elseif($age >= 59 ){
+    echo "adult";
+}
+else ($age >= 60){
+    echo "seniorcitizen";
+}
+
+
+?>
