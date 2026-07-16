845e685325fb7e930b61dd4b9221b18d7eb6e79c
hamody1818 <hamodycatli18@gmail.com>
2026-07-16 17:06:51 +0800

    Add files via upload

---
+++ hamody2d/process.php
@@ -0,0 +1,9 @@
+<?php
+$Name = $_GET["Fullname"];
+$Email = $_GET["Email"];
+$Message = $_GET["Message"];
+
+echo "Fullname $Name <br>";
+echo "Email: $Email <br>";
+echo "Message: $Message <br>";
+?>
